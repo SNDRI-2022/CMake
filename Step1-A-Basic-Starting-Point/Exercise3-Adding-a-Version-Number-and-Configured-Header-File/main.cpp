0ffbbc7dab7b41f9ea2bc5294b03a0de9c49e0c8
liu2830 <2305195328@qq.com>
2022-11-17 20:52:57 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Step1-A-Basic-Starting-Point/Exercise2-Specifying-the-C++-Standard/build/CXX11.exe
    	new file:   Step1-A-Basic-Starting-Point/Exercise3-Adding-a-Version-Number-and-Configured-Header-File/CMakelists.txt
    	new file:   Step1-A-Basic-Starting-Point/Exercise3-Adding-a-Version-Number-and-Configured-Header-File/README.md
    	new file:   Step1-A-Basic-Starting-Point/Exercise3-Adding-a-Version-Number-and-Configured-Header-File/main.cpp

---
+++ Step1-A-Basic-Starting-Point/Exercise3-Adding-a-Version-Number-and-Configured-Header-File/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+int main(int argv, char **argc)
+{
+    const std::vector<long long> test{123456789, 987654321, 1029384756};
+    for (auto i = test.begin(); i != test.end(); i++)
+        std::cout << *i << std::endl;
+    return 0;
+}
