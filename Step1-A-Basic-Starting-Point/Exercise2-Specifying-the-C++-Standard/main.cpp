d6d1505a6c04b5baea8e3f0d313a760fcc5567de
liu2830 <2305195328@qq.com>
2022-11-17 10:51:34 +0800

    modified:   Step1-A-Basic-Starting-Point/Exercise2-Specifying-the-C++-Standard/CMakeLists.txt 	modified:   Step1-A-Basic-Starting-Point/Exercise2-Specifying-the-C++-Standard/build/CXX11.exe 	modified:   Step1-A-Basic-Starting-Point/Exercise2-Specifying-the-C++-Standard/main.cpp

--- Step1-A-Basic-Starting-Point/Exercise2-Specifying-the-C++-Standard/main.cpp
+++ Step1-A-Basic-Starting-Point/Exercise2-Specifying-the-C++-Standard/main.cpp
@@ -5,8 +5,6 @@ int main(int argv, char **argc)
 {
     const std::vector<long long> test{123456789, 987654321, 1029384756};
     for (auto i = test.begin(); i != test.end(); i++)
-    {
         std::cout << *i << std::endl;
-    }
     return 0;
 }
